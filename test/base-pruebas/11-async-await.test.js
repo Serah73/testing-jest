bfbc68e17d09c69a530ce412bab6cdd4262354e6
José Ramón Rodríguez Hernández <joserrh97@gmail.com>
2022-09-30 11:11:30 +0100

    chore

--- test/base-pruebas/11-async-await.test.js
+++ test/base-pruebas/11-async-await.test.js
@@ -3,7 +3,7 @@ import { getImagen } from '../../src/base-pruebas/11-async-await'
 describe( 'Pruebas 11-async', () => {
     test('Debe devolver una url', async () => {
         const url = await getImagen()
-        console.log(url)
-        //expect( typeof url ).toBe('string')
+    
+        expect( typeof url ).toBe('string')
     })
 })
